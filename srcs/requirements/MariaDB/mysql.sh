d11749bc3ab553b191f6d9669d0e1217da99c984
denis <denis.c1@live.fr>
2022-06-15 18:07:57 +0200

    finished

--- srcs/requirements/MariaDB/mysql.sh
+++ srcs/requirements/MariaDB/mysql.sh
@@ -4,9 +4,7 @@ touch /run/openrc/softlevel
 MYSQL_HOST=""
 /etc/init.d/mariadb setup
 rc-status
-#mysql_install_db --datadir=/home/dchheang/data --user=mysql --rpm > /dev/null
 /etc/init.d/mariadb start
-#mysql -u root -p '$WP_DATABASE_PWD' < /home/docker/script/database.sh
 sudo mysql -e "CREATE USER IF NOT EXISTS 'user42'@'%' IDENTIFIED BY '${MYSQL_ROOT_PWD}'"
 sudo mysql < /home/docker/script/database.sh 
 
