ec93836a9e68b60b553d80dbf8017557265cd6a0
denis <denis.c1@live.fr>
2022-06-27 08:21:16 +0200

    clean to do

--- srcs/requirements/MariaDB/mysql.sh
+++ srcs/requirements/MariaDB/mysql.sh
@@ -3,10 +3,12 @@ touch /run/openrc/softlevel
 
 MYSQL_HOST=""
 /etc/init.d/mariadb setup
+chown -R mysql:mysql /usr/bin/mysqld
 rc-status
 /etc/init.d/mariadb start
-sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '${MYSQL_ROOT_PWD}'"
-sudo mysql < "FLUSH PRIVILEGES"
+mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PWD}'; FLUSH PRIVILEGES;"
+#mysql -uroot -p${MYSQL_ROOT_PWD} < "FLUSH PRIVILEGES"
+#mysql_secure_installation -p{$MYSQL_ROOT_PWD} --skip-grant-tables -D
 mysql -uroot -p${MYSQL_ROOT_PWD} -e "CREATE USER IF NOT EXISTS 'user42'@'%' IDENTIFIED BY '${MYSQL_ROOT_PWD}'"
 mysql -uroot -p${MYSQL_ROOT_PWD} < /home/docker/script/database.sh 
 
--- srcs/requirements/WordPress/wp.sh
+++ srcs/requirements/WordPress/wp.sh
@@ -6,7 +6,7 @@ while ! mariadb -h$MYSQL_HOST -u$WP_DATABASE_USR -p$WP_DATABASE_PWD &>/dev/null;
 	sleep 3
 done
 
-wp core install --url=$DOMAIN_NAME/wordpress --title="test" --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email="admin@test.fr" --path=/var/www/html/wordpress/
+wp core install --url=$DOMAIN_NAME --title="test" --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email="admin@test.fr" --path=/var/www/html/wordpress/
 
 wp user create author author@test.fr --role=author --path=/var/www/html/wordpress/
 wp user create author2 author2@test.fr --role=author --path=/var/www/html/wordpress/
